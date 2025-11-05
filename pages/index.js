0d717c6db65e09266d187d0aabc8743300b61447
Fanuel Shemeles <154880060+fanishemeles@users.noreply.github.com>
2025-11-05 21:45:59 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
-import logo from "../public/logo.png";
+
 export default function Home() {
   return (
     <>
